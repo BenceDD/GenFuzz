db081b139dcfe1e786a93e1af16a4d1375d61bdd
Mikulás Bence <mikulasbence@gmail.com>
2018-04-19 22:57:17 +0200

    Added new API methods for MattermostRequests

--- mrequests.py
+++ mrequests.py
@@ -28,34 +28,40 @@ class MattermostRequests:
         # Check parameters
         for param in api_params:
             if 'required' in param and (param['required'] is True or type(param['required']) is list):
-                # Quers string parameters
-                if param['in'] == 'query' and param['name'] not in kwargs:
+                # Path parameters
+                if param['in'] == 'path' and param['name'] not in kwargs:
                     raise Exception('Missing argument: ' + param['name'])
                 # Body parameters
-                if param['in'] == 'body':
+                if param['in'] == 'body' and 'required' in param['schema']:
                     for sch_param in param['schema']['required']:
                         if sch_param not in kwargs:
                             raise Exception('Missing argument: ' + sch_param)
-        
-        # Set body parameters
-        query_params = {}
-        for param in api_params:
-            if param['in'] == 'body':
-                query_params.update({ name: kwargs[name] for name, _ in param['schema']['properties'].items() if name in kwargs})
+                # Query string parameters
+                if param['in'] == 'query' and 'required' in param and param['required'] is True and param['name'] not in kwargs:
+                    raise Exception('Missing argument: ' + param['name'])
 
-        # Set parameters header (only the auth header...)
-        auth_header = kwargs['auth_header'] if 'auth_header' in kwargs else None
-        
         # Set query parameters
         url = path.format(**kwargs)
+
+        # Set query string parameters
+        query_params = { param['name']: kwargs[param['name']] for param in api_params if param['in'] == 'query' and param['name'] in kwargs } or None
+
+        # Set parameters header (currently only the auth header...)
+        auth_header = kwargs['auth_header'] if 'auth_header' in kwargs else None
         
+        # Set body parameters
+        body_params = {}
+        for param in api_params:
+            if param['in'] == 'body':
+                body_params.update({ name: kwargs[name] for name, _ in param['schema']['properties'].items() if name in kwargs})
+
         # Send requests
         if method.lower() == 'get':
-            response = requests.get(self.api_url + url, headers=auth_header)
+            response = requests.get(self.api_url + url, params=query_params, headers=auth_header)
         elif method.lower() == 'post':
-            response = requests.post(self.api_url + url, data=json.dumps(query_params), headers=auth_header)
+            response = requests.post(self.api_url + url, params=query_params, headers=auth_header, data=json.dumps(body_params))
         elif method.lower() == 'put':
-            response = requests.put(self.api_url + url, data=json.dumps(query_params), headers=auth_header)
+            response = requests.put(self.api_url + url, params=query_params, headers=auth_header, data=json.dumps(body_params))
         
         # Parse result and return
         if get_headers:  
@@ -74,8 +80,21 @@ class MattermostRequests:
     def post_message_to_channel(self, auth_header, channel_id, message):
         return self.send_request('/posts', 'post', channel_id=channel_id, message=message, auth_header=auth_header)
 
-    def get_users_info(self, auth_header):
-        return self.send_request('/users', 'get', auth_header=auth_header)
+    def get_users_info(self, auth_header, **kwargs):
+        return self.send_request('/users', 'get', auth_header=auth_header, **kwargs)
 
     def get_user_info(self, auth_header, user_id):
         return self.send_request('/users/{user_id}', 'get', user_id=user_id, auth_header=auth_header)
+
+    def update_user_info(self, auth_header, user_id, **kwargs):
+        return self.send_request('/users/{user_id}/patch', 'put', user_id=user_id, auth_header=auth_header, **kwargs)
+
+    def get_teams(self, auth_header, **kwargs):
+        return self.send_request('/teams', 'get', auth_header=auth_header, **kwargs)
+
+    def get_team(self, auth_header, team_id):
+        return self.send_request('/teams/{team_id}', 'get', team_id=team_id, auth_header=auth_header)
+
+    def update_team(self, auth_header, team_id, **kwargs):
+        return mr.send_request('/teams/{team_id}/patch', 'put', auth_header=auth_header, team_id=team_id, **kwargs)
+
