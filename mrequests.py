13a099983913667b2caf51abdf39cb8a784ef335
Mikulás Bence <mikulasbence@gmail.com>
2018-04-23 22:43:30 +0200

    Mattermost API methods implemented by their summaries

--- mrequests.py
+++ mrequests.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import yaml
+import re
 
 class MattermostRequests:
 
@@ -53,7 +54,7 @@ class MattermostRequests:
         # Set parameters header (currently only the auth header...)
         auth_header = kwargs['auth_header'] if 'auth_header' in kwargs else self.auth_header
         if auth_header is None:
-            print('Warning: No auth header is passed...')
+            print('No auth header is passed...')
         
         # Set body parameters
         body_params = {}
@@ -64,8 +65,10 @@ class MattermostRequests:
                 if '$ref' in schema:  # schema is given by reference....
                     ref_type, ref_name = tuple(schema['$ref'].split('/')[1:])
                     schema = self.api_data[ref_type][ref_name]
+                if schema['type'] != 'object':
+                    raise Exception('Passing non object arguments are not implemented!')
 
-                body_params.update({ name: kwargs[name] for name, _ in schema['properties'].items() if name in kwargs })
+                body_params.update({ name: kwargs[name] for name in schema['properties'] if name in kwargs })
         body_params = body_params or None        
 
         # Send requests
@@ -77,67 +80,26 @@ class MattermostRequests:
             return response.headers, json.loads(response.text)
         else:
             return json.loads(response.text)
-
-    def get_team_id(self, auth_header):
-        data = self.send_request('/teams', 'get', auth_header=auth_header)
-        return data[0]['id']
-
-    def get_channel_list(self, auth_header, team_id):
-        data = self.send_request('/teams/{team_id}/channels', 'get', team_id=team_id, auth_header=auth_header)
-        return { x['id']: x['name'] for x in data }
-
-    def post_message_to_channel(self, auth_header, channel_id, message):
-        return self.send_request('/posts', 'post', channel_id=channel_id, message=message, auth_header=auth_header)
-
-    def get_users(self, **kwargs):
-        """ Get a page of a list of users. Based on query string parameters, select users from a team,
-            channel, or select users not in a specific channel. """
-        return self.send_request('/users', 'get', **kwargs)
-
-    def get_user(self, **kwargs):
-        """ Get a user a object. Sensitive information will be sanitized out. """
-        return self.send_request('/users/{user_id}', 'get', **kwargs)
-
-    def update_user(self, **kwargs):
-        """ Partially update a user by providing only the fields you want to update. Omitted fields will 
-            not be updated. The fields that can be updated are defined in the request body, all other provided 
-            fields will be ignored. """
-        return self.send_request('/users/{user_id}/patch', 'put', **kwargs)
-
-    def get_teams(self, **kwargs):
-        """ For regular users only returns open teams. Users with the "manage_system" permission will 
-            return teams regardless of type. """
-        return self.send_request('/teams', 'get', **kwargs)
-
-    def get_team(self, **kwargs):
-        """ Get a team on the system. """
-        return self.send_request('/teams/{team_id}', 'get', **kwargs)
-
-    def update_team(self, **kwargs):
-        """ Partially update a team by providing only the fields you want to update. Omitted fields will not 
-            be updated. The fields that can be updated are defined in the request body, all other provided 
-            fields will be ignored. """
-        return self.send_request('/teams/{team_id}/patch', 'put', **kwargs)
-
-    def get_team_members(self, **kwargs):
-        """ Get a page team members list based on query string parameters - team id, page and per page. """
-        return self.send_request('/teams/{team_id}/members', 'get', **kwargs)
-
-    def create_team(self, **kwargs):
-        """ Create a new team on the system. """
-        return self.send_request('/teams', 'post', **kwargs)
-
-    def delete_team(self, **kwargs):
-        """ Soft deletes a team, by marking the team as deleted in the database. Soft deleted teams will 
-            not be accessible in the user interface. """
-        return self.send_request('/teams/{team_id}', 'delete', **kwargs)
-
-
-    def add_user_to_team(self, **kwargs):
-        """ Add user to the team by user_id. """
-        return self.send_request('/teams/{team_id}/members', 'post', **kwargs)
-
-    def remove_user_from_team(self, **kwargs):
-        """ Delete the team member object for a user, effectively removing them from a team. """
-        return self.send_request('/teams/{team_id}/members/{user_id}', 'delete', **kwargs)
     
+    def __convert_name(self, name):
+        return re.sub('[^a-zA-Z ]+', '', name + ' ')[:-1].replace(' ', '_').lower()
+
+    def __getattr__(self, name):
+        for path, methods in self.api_data['paths'].items():
+            for method, method_params in methods.items():
+                if self.__convert_name(method_params['summary']) == name:
+                    try:  # There are multiple methods with same name but with different params.
+                        return lambda **kwargs: self.send_request(path, method, **kwargs)
+                    except:
+                        pass
+        
+        raise AttributeError('There is no such method: ' + name + ' (All exceptions catched...)')
+
+    def get_methods(self):
+        methods = []
+        for path, http_methods in self.api_data['paths'].items():
+            for method, method_params in http_methods.items():
+                params = [p['name'] for p in method_params['parameters']] if 'parameters' in method_params else []
+                methods.append((self.__convert_name(method_params['summary']), params))
+        return sorted(methods)
+
