6f8dbc0bfa136a55b218cc546fbb6f80c075c67b
Mikulás Bence <mikulasbence@gmail.com>
2018-04-26 19:15:57 +0200

    YAMLHandler resolves all refs

--- yamlhandler.py
+++ yamlhandler.py
@@ -1,5 +1,6 @@
 import yaml
 import re
+import copy
 
 class YAMLHandler:
 
@@ -12,33 +13,39 @@ class YAMLHandler:
 
     def __follow_ref(self, ref):
         ref_type, ref_name = tuple(ref.split('/')[1:])
+        return self.api_data[ref_type][ref_name]   
+
+    def __replace_ref(self, obj):
+        while(self.__replace_ref_recursively(obj)):
+            pass
+        return obj
+
+    def __follow_ref(self, ref):
+        ref_type, ref_name = tuple(ref.split('/')[1:])
         return self.api_data[ref_type][ref_name]
-
-    def __create_param(self, in_field, required, param_type):
-        return {
-            'in': in_field,
-            'required': required is True,
-            'type': param_type
-        }
-
-    def __process_schema(self, schema):
-        if '$ref' in schema['schema']:
-            schema['schema'] = self.__follow_ref(schema['schema']['$ref'])
-            
-        parameters = {}
         
-        if schema['schema']['type'] == 'object':
-            required_params = schema['schema']['required'] if 'required' in schema['schema'] and type(schema['schema']['required']) is list else []
-            
-            for name, desc in schema['schema']['properties'].items():
-                if '$ref' in desc:
-                    desc = self.__follow_ref(desc['$ref'])  # TODO!!
-                parameters[name] = self.__create_param(schema['in'], name in required_params, desc['type'])
-        
-        elif schema['schema']['type'] == 'array':
-            pass  # TODO!
-            
-        return parameters
+    def __replace_ref_recursively(self, obj):
+        if type(obj) is list:
+            for elem in obj:
+                if self.__replace_ref_recursively(elem):
+                    return True
+                
+        elif type(obj) is dict:
+            if '$ref' in obj:
+                obj.update(copy.deepcopy(self.__follow_ref(obj['$ref'])))
+                obj.pop('$ref')
+                return True
+            else:
+                for dv in obj.values():  # dict value
+                    if self.__replace_ref_recursively(dv):
+                        return True          
+        return False
+
+    def get_request_params(self, path, method_type):
+        return self.__replace_ref(copy.deepcopy(self.api_data['paths'][path][method_type]['parameters']))
+
+    def get_response_params(self, path, method_type):
+        return self.__replace_ref(copy.deepcopy(self.api_data['paths'][path][method_type]['responses']))
 
     def get_matching_methods(self, name):
         selected_methods = []  # There are multiple methods with same name but with different params.
@@ -48,25 +55,10 @@ class YAMLHandler:
                     selected_methods.append((path, method))           
         return selected_methods
 
-    def get_call_params(self, path, method_type):
-        parameters = {}
-        method = self.api_data['paths'][path][method_type]
-        if 'parameters' in method:      
-            for param in method['parameters']:
-                if 'schema' in param:
-                    parameters.update(self.__process_schema(param))
-                else:
-                    required = 'required' in param and param['required'] is True
-                    parameters[param['name']] = self.__create_param(param['in'], required, param['type'])
-        return parameters
-
     def get_methods(self):
         methods = []
         for path, method in self.api_data['paths'].items():
             for method_name, method_params in method.items():
                 methods.append((self.__convert_name(method_params['summary']), self.get_call_params(path, method_name)))
         return sorted(methods, key=lambda x: x[0])
-        
-    def get_return_params(self, path, method):
-        pass
 
